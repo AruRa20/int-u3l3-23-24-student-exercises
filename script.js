ef2d14b532b1df1633a6173c54158b697064e1f9
AruRa20 <149088263+AruRa20@users.noreply.github.com>
2024-02-15 16:23:28 +0000

    event listeners

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let button = document.querySelector("button");
+let answer = document.querySelector(".answer");
                                     
 // 1. Declare a variable called `answer` that stores the p tag that has the class name `answer`.
 
 
 
-button.addEventListener("click", function() {
+answer.addEventListener("click", function() {
 // 2. Replace the value with a string with a name of a movie you enjoy.
 // 3. Uncomment the line of code below, run, and try the button.
 // answer.innerHTML="movie name goes here";
 
-
-
+answer.innerHTML="harry potter";
 
 
 });
